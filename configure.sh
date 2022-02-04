8ac22f3d34a24ce7cb8c6f4792ca78e89b30209e
jinjp <jinjp@126.com>
2022-02-04 20:26:10 +0800

    'memo03'

--- configure.sh
+++ configure.sh
@@ -1,14 +1,18 @@
+echo debug01
 mkdir /tmp/v2ray
 wget -q https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray/v2ray.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+echo debug02
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
 install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+echo debug03
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
 
 # V2Ray new configuration
 install -d /usr/local/etc/v2ray
+echo debug05
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
@@ -36,6 +40,8 @@ cat << EOF > /usr/local/etc/v2ray/config.json
     ]                       
 }  
 EOF                         
-                             
+echo debug06
+
 # Run V2Ray                  
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+echo debug07
